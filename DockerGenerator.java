832c33ac53011094f17dc58137efc8edc811ac19
saiforacle <mohammad.s.saif@oracle.com>
2021-11-22 18:26:31 +0530

    Requirement

--- DockerGenerator.java
+++ DockerGenerator.java
@@ -7,12 +7,44 @@ import java.util.Properties;
 public class DockerGenerator {
 	public static void main(String[] args) throws IOException {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-FileInputStream fin=new FileInputStream("C:\\Users\\MOSAIF\\BDD\\CucumberJava\\jenkinsprop.properties");
+FileInputStream fin=new FileInputStream("C:\\Users\\MOSAIF\\Documents\\GitHub\\JenkinsBDD\\jenkinsprop.properties");
 Properties prop=new Properties();
 prop.load(fin);
-String tags=prop.getProperty("tags");
+String type=prop.getProperty("type");
+
+System.out.println(type);
+
+String feature=prop.getProperty("Feature_name");
+
+System.out.println(feature);
+String scenario=prop.getProperty("Scenario_name");
+
+System.out.println(scenario);
+String tags = null;
+try {
+	
+	Class.forName("oracle.jdbc.driver.OracleDriver");  
+	Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@Whf00axl:1521:USFEDDB","QAUTIL","QAUTIL");
+	Statement stmt=conn.createStatement();  
+	//System.out.println("Connected");
+	//   
+	//step4 execute query  
+	ResultSet rs=stmt.executeQuery("select distinct tags from parametertable where type='"+type+"' and feature_name='"+feature+"' and scenario_id='"+scenario+"' ");  
+while (rs.next())
+{
+	System.out.println("inside");
+	tags=rs.getString(1);
+}
+System.out.println("Tags="+tags);
+}
+catch(Exception e)
+{
+	e.printStackTrace();
+}
+
 System.out.println(tags);
-FileWriter fout=new FileWriter("C:\\Users\\MOSAIF\\BDD\\CucumberJava\\Dockerfile");
+
+FileWriter fout=new FileWriter("C:\\Users\\MOSAIF\\Documents\\GitHub\\JenkinsBDD\\Dockerfile");
 fout.write("#base image of JDK(First Layer)\r\n"
 		+ "FROM openjdk\r\n"
 		+ "\r\n"
